dd76a22a75d12bd64ba55730f91c6563e1de87ac
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2022-09-22 17:08:55 +0300

    Add video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+'use strict';
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+function playedTime(data) {
+  const { seconds } = data;
+  localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+}
+
+const throttledPlayedTime = throttle(playedTime, 1000);
+
+player.on('timeupdate', throttledPlayedTime);
+
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+player.setCurrentTime(currentTime);
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
