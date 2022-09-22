d67fa826fa2c4c578600ab5c3baeb91755eea1dd
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2022-09-22 15:32:58 +0300

    Add simplLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMurkup = galleryCreateMurkup(galleryItems);
+
+function galleryCreateMurkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMurkup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captionDelay: '250',
+});
